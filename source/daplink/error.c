makerdiary/DAPLink

Support for disabling inclusion of error message strings.
- If DAPLINK_NO_ERROR_MESSAGES is defined then the fail.txt file will
  include only "Error NN" instead of a descriptive error message.
- This is required for the Musca-B1 builds to fit in flash with gcc.

source/daplink/error.c
@@ -3,7 +3,7 @@
  * @brief   Implementation of error.h
  *
  * DAPLink Interface Firmware
- * Copyright (c) 2009-2016, ARM Limited, All Rights Reserved
+ * Copyright (c) 2009-2019, ARM Limited, All Rights Reserved
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -19,11 +19,13 @@
  * limitations under the License.
  */
 
+#include <stdlib.h>
 #include "error.h"
 #include "util.h"
 #include "macro.h"
 #include "compiler.h"
 
+#if !DAPLINK_NO_ERROR_MESSAGES
 static const char *const error_message[] = {
 
     /* Shared errors */
@@ -124,6 +126,10 @@ static const char *const error_message[] = {
 
 };
 
+COMPILER_ASSERT(ERROR_COUNT == ELEMENTS_IN_ARRAY(error_message));
+
+#endif // DAPLINK_NO_ERROR_MESSAGES
+
 static error_type_t error_type[] = {
 
     /* These should always stay the same for each error type. */
@@ -223,11 +229,12 @@ static error_type_t error_type[] = {
     ERROR_TYPE_INTERFACE,
 };
 
-COMPILER_ASSERT(ERROR_COUNT == ELEMENTS_IN_ARRAY(error_message));
+COMPILER_ASSERT(ERROR_COUNT == ELEMENTS_IN_ARRAY(error_type));
 
 const char *error_get_string(error_t error)
 {
-    const char *msg = 0;
+#if !DAPLINK_NO_ERROR_MESSAGES
+    const char *msg = NULL;
 
     if (error < ERROR_COUNT) {
         msg = error_message[error];
@@ -239,6 +246,15 @@ const char *error_get_string(error_t error)
     }
 
     return msg;
+#else // DAPLINK_NO_ERROR_MESSAGES
+    static char error_num_str[10] = "Error 00";
+#define ERROR_NUM_CHAR_INDEX (6) // offset of first '0' in error_num_str[]
+
+    error_num_str[ERROR_NUM_CHAR_INDEX+0] = '0' + ((int)error / 1000) % 10;
+    error_num_str[ERROR_NUM_CHAR_INDEX+1] = '0' + ((int)error / 100) % 10;
+    COMPILER_ASSERT(ERROR_COUNT < 100); // if this assert is hit, add a digit here
+    error_num_str[ERROR_NUM_CHAR_INDEX+2] = 0;
+#endif  // DAPLINK_NO_ERROR_MESSAGES
 }
 
 error_type_t error_get_type(error_t error)

google/DAPLink-port

source/daplink/error.c
@@ -3,7 +3,7 @@
  * @brief   Implementation of error.h
  *
  * DAPLink Interface Firmware
- * Copyright (c) 2009-2016, ARM Limited, All Rights Reserved
+ * Copyright (c) 2009-2020, ARM Limited, All Rights Reserved
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -19,10 +19,12 @@
  * limitations under the License.
  */
 
+#include <stdlib.h>
 #include "error.h"
 #include "util.h"
 #include "compiler.h"
 
+#if !DAPLINK_NO_ERROR_MESSAGES
 static const char *const error_message[] = {
 
     /* Shared errors */
@@ -123,6 +125,10 @@ static const char *const error_message[] = {
 
 };
 
+COMPILER_ASSERT(ERROR_COUNT == ARRAY_SIZE(error_message));
+
+#endif // DAPLINK_NO_ERROR_MESSAGES
+
 static error_type_t error_type[] = {
 
     /* These should always stay the same for each error type. */
@@ -222,11 +228,12 @@ static error_type_t error_type[] = {
     ERROR_TYPE_INTERFACE,
 };
 
-COMPILER_ASSERT(ERROR_COUNT == ARRAY_SIZE(error_message));
+COMPILER_ASSERT(ERROR_COUNT == ARRAY_SIZE(error_type));
 
 const char *error_get_string(error_t error)
 {
-    const char *msg = 0;
+#if !DAPLINK_NO_ERROR_MESSAGES
+    const char *msg = NULL;
 
     if (error < ERROR_COUNT) {
         msg = error_message[error];
@@ -238,6 +245,15 @@ const char *error_get_string(error_t error)
     }
 
     return msg;
+#else // DAPLINK_NO_ERROR_MESSAGES
+    static char error_num_str[10] = "Error 00";
+#define ERROR_NUM_CHAR_INDEX (6) // offset of first '0' in error_num_str[]
+
+    error_num_str[ERROR_NUM_CHAR_INDEX+0] = '0' + ((int)error / 1000) % 10;
+    error_num_str[ERROR_NUM_CHAR_INDEX+1] = '0' + ((int)error / 100) % 10;
+    COMPILER_ASSERT(ERROR_COUNT < 100); // if this assert is hit, add a digit here
+    error_num_str[ERROR_NUM_CHAR_INDEX+2] = 0;
+#endif  // DAPLINK_NO_ERROR_MESSAGES
 }
 
 error_type_t error_get_type(error_t error)