4e28c64d50afc68b32a4a3ac4b809927abe3ef2c
Aaron Okano <aaronokano@google.com>
2022-03-03 18:06:28 +0800

    Update Jenkins to build for "freertos" and with GCC 7.3.1
    Seems that our build infrastructure will use the "platform" argument to
    decide which type of host to build on. When set to "linux", it builds on
    some Debian host with some sort of janky arm-none-eabi-gcc from a folder
    "/tmp/building". Instead of doing that, switch to "freertos", which will
    build in the usual embedded environment and includes all the toolchains
    we use for other projects. I chose GCC 7.3.1, since I think that's the
    latest one available (I wonder if this can be updated).
    
    bug: 222442977
    Change-Id: I4fa3406c223e3f40625a6312a26480fca139a76b

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -13,6 +13,8 @@ pushd "$REPO_ROOT"
 python3 ./tools/add_macro_to_record.py ./records/board/stm32h743ii_udb_if.yaml "UDB_VERSION_BASE=${bamboo_VersionBase}"
 python3 ./tools/add_macro_to_record.py ./records/board/stm32h743ii_udb_if.yaml "UDB_BUILD_NUMBER=${bamboo_buildNumber}"
 
+export ARM_GCC_PATH=/usr/local/nestlabs/toolchains/gcc-arm-7.3.1/bin/
+
 #build generic daplink interface and bootloader applications to check they still compile
 python3 ./tools/progen_compile.py -t make_gcc_arm stm32h743ii_if --clean --parallel
 python3 ./tools/progen_compile.py -t make_gcc_arm stm32h743ii_bl --clean --parallel
