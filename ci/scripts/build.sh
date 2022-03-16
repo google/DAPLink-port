5a54335e764cbb3e86d0d17f501809b62db5e916
Gabor Csapo <gaborcsapo@google.com>
2022-03-16 12:03:52 +0800

    Separate UDB from stm32h743 in project.yaml
    Just a small cleanup task that should make our lives easier when we add
    macros and when we get to opensourcing the project.
    
    In the past we've been adding UDB functionality to the generic stm32h743
    yaml. Ideally the two should be able to live separately, because from
    DAPLink's perspective not all stm32h743ii is a UDB.
    
    Change-Id: I04bf1b980fdd3e719548905e1c8ea58e50034cc6

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -10,11 +10,15 @@ python3 -m pip install -r "${REPO_ROOT}/requirements.txt"
 
 pushd "$REPO_ROOT"
 
-python3 ./tools/add_macro_to_record.py ./records/hic_hal/stm32h743ii.yaml "UDB_VERSION_BASE=${bamboo_VersionBase}"
-python3 ./tools/add_macro_to_record.py ./records/hic_hal/stm32h743ii.yaml "UDB_BUILD_NUMBER=${bamboo_buildNumber}"
+python3 ./tools/add_macro_to_record.py ./records/board/stm32h743ii_udb_if.yaml "UDB_VERSION_BASE=${bamboo_VersionBase}"
+python3 ./tools/add_macro_to_record.py ./records/board/stm32h743ii_udb_if.yaml "UDB_BUILD_NUMBER=${bamboo_buildNumber}"
 
-#build interface application
+#build generic daplink interface and bootloader applications to check they still compile
 python3 ./tools/progen_compile.py -t make_gcc_arm stm32h743ii_if --clean --parallel
-
-#build bootloader
 python3 ./tools/progen_compile.py -t make_gcc_arm stm32h743ii_bl --clean --parallel
+
+#build UDB daplink interface application
+python3 ./tools/progen_compile.py -t make_gcc_arm stm32h743ii_udb_if --clean --parallel
+
+#build UDB bootloader
+python3 ./tools/progen_compile.py -t make_gcc_arm stm32h743ii_udb_bl --clean --parallel
